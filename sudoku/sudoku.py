045ae8940ffe6973e0effd13330a9de76ce16838
HankChow <hankchow@hankchow.net>
2017-10-21 16:40:57 +0800

    optimize the initial of cells

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -7,16 +7,20 @@ class Cell(object):
         self.row = r
         self.column = c
         self.value = v
+        self.nominees = [n for n in range(1, 10)]
+
+    def confirm(self, v):
+        self.value = v
         self.nominees = []
 
 class Sudoku(object):
 
     def __init__(self):
-        self.cells = []
+        self.cells = {}
         for r in range(9):
             for c in range(9):
                 cell = Cell(r, c)
-                self.cells.append(cell)
+                self.cells[(r, c)] = cell
 
     def read_quiz(self):
         pass
