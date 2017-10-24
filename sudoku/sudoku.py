a2471579d72bbb41bdb778d8ab5bd2b085aab82b
HankChow <hankchow@hankchow.net>
2017-10-24 17:14:17 +0800

    add method of unique nominee

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 from colorama import *
+from functools import reduce
 
 class Cell(object):
     
@@ -26,6 +27,7 @@ class Cell(object):
         if len(self.nominees) == 1 and self.value == 0:
             self.value = self.nominees[0]
             self.nominees = []
+        return self.value
 
 
 class Sudoku(object):
@@ -54,7 +56,7 @@ class Sudoku(object):
             row = self.get_cells_by_row(i)
             for j in range(9):
                 print(('' if row[j].block % 2 == 0 else (Fore.BLACK + Back.WHITE)) + 
-                    ('' if row[j].is_given else Fore.GREEN) + 
+                    ('' if row[j].is_given else Fore.BLUE + Style.BRIGHT) + 
                     str(row[j].value if row[j].value != 0 else ' '), end=' ')
                 print(Style.RESET_ALL, end='')
             print()
@@ -87,6 +89,14 @@ class Sudoku(object):
                     count += 1
         return count
 
+    # 当一个 cell 确定后，必须调用此方法，去除与该 cell 同一 row/column/block 的其它 cell 的候选数
+    def suppress(self, cell, value):
+        for row in range(9):
+            for column in range(9):
+                if row == cell.row or column == cell.column or self.cells[(row, column)].block == cell.block:
+                    if value in self.cells[(row, column)].nominees:
+                        self.cells[(row, column)].nominees.remove(value)
+
     # 遍历每一个 cell 并去除候选数，然后遍历 cell.clear() 一次
     def kill_nominees(self):
         previous_unsolved = None
@@ -94,6 +104,7 @@ class Sudoku(object):
         while(previous_unsolved != unsolved):
             for row in range(9):
                 for column in range(9):
+                    # 当某个 cell 为空时
                     if self.cells[(row, column)].value == 0:
                         nominee_set = set(self.cells[(row, column)].nominees)
                         others_in_row = set(list(map(lambda x: x.value, self.get_cells_by_row(row))))
@@ -102,14 +113,70 @@ class Sudoku(object):
                         self.cells[(row, column)].nominees = list(nominee_set - others_in_row - others_in_column - others_in_block)
             for row in range(9):
                 for column in range(9):
-                    self.cells[(row, column)].clear()
+                    newly_fill = self.cells[(row, column)].clear()
+                    self.suppress(self.cells[(row, column)], newly_fill)
             previous_unsolved = unsolved
             unsolved = self.get_unsolved_count()
+
+    # 遍历每一个 row/column/block ，如果 row/column/block 内唯一有一个未填的 cell 拥有某个候选数，这个未填的 cell 就可以 confirm 这个候选数
+    def unique_nominee(self):
+        previous_unsolved = None
+        unsolved = self.get_unsolved_count()
+        while(previous_unsolved != unsolved):
+            # row
+            for row in range(9):
+                empty_cells = list(filter(lambda x: x.value == 0, self.get_cells_by_row(row)))
+                if len(empty_cells) > 1:
+                    for index, value in enumerate(empty_cells):
+                        others_nominees = set(reduce(lambda x, y: x + y, list(map(lambda w: w.nominees, list(filter(lambda z: empty_cells.index(z) != index, empty_cells))))))
+                        this_nominees = list(set(value.nominees) - others_nominees)
+                        if len(this_nominees) == 1:
+                            unique_cell = empty_cells[index]
+                            self.cells[(unique_cell.row, unique_cell.column)].confirm(this_nominees[0])
+                            self.suppress(self.cells[(unique_cell.row, unique_cell.column)], this_nominees[0])
+            # column
+            for column in range(9):
+                empty_cells = list(filter(lambda x: x.value == 0, self.get_cells_by_column(column)))
+                if len(empty_cells) > 1:
+                    for index, value in enumerate(empty_cells):
+                        others_nominees = set(reduce(lambda x, y: x + y, list(map(lambda w: w.nominees, list(filter(lambda z: empty_cells.index(z) != index, empty_cells))))))
+                        this_nominees = list(set(value.nominees) - others_nominees)
+                        if len(this_nominees) == 1:
+                            unique_cell = empty_cells[index]
+                            self.cells[(unique_cell.row, unique_cell.column)].confirm(this_nominees[0])
+                            self.suppress(self.cells[(unique_cell.row, unique_cell.column)], this_nominees[0])
+            # block
+            for block in range(9):
+                empty_cells = list(filter(lambda x: x.value == 0, self.get_cells_by_block(block)))
+                if len(empty_cells) > 1:
+                    for index, value in enumerate(empty_cells):
+                        others_nominees = set(reduce(lambda x, y: x + y, list(map(lambda w: w.nominees, list(filter(lambda z: empty_cells.index(z) != index, empty_cells))))))
+                        this_nominees = list(set(value.nominees) - others_nominees)
+                        if len(this_nominees) == 1:
+                            unique_cell = empty_cells[index]
+                            self.cells[(unique_cell.row, unique_cell.column)].confirm(this_nominees[0])
+                            self.suppress(self.cells[(unique_cell.row, unique_cell.column)], this_nominees[0])
+            previous_unsolved = unsolved
+            unsolved = self.get_unsolved_count()
+
+
+    # 按照优先级，往复遍历一次所有求值方法
+    def whole_solve(self):
+        priority = [self.kill_nominees, self.unique_nominee]
+        whole = priority[:-1] + priority[::-1]
+        previous_unsolved = None
+        unsolved = self.get_unsolved_count()
+        while(previous_unsolved != unsolved):
+            for method in whole:
+                method()
+            previous_unsolved = unsolved
+            unsolved = self.get_unsolved_count()
+
             
 if __name__ == '__main__':
     s = Sudoku()
     s.read_sudoku('aaa')
     s.display_sudoku()
-    s.kill_nominees()
+    s.whole_solve()
     print()
     s.display_sudoku()
