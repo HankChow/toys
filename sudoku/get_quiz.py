e5a05d4897c1d1294f94f516ce5f95602ae4f4f5
HankChow <hankchow@hankchow.net>
2017-11-01 23:42:44 +0800

    add cheating in get_quiz.py

--- sudoku/get_quiz.py
+++ sudoku/get_quiz.py
@@ -2,11 +2,16 @@
 # coding: utf-8
 
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
-    url = 'http://www.oubk.com/DailySudoku/17465/5'
-    page = requests.get(url).text
+    if 'page' not in sys.argv:
+        url = 'http://www.oubk.com/DailySudoku/17465/5'
+        page = requests.get(url).text
+    else:
+        with open('page') as f:
+            page = f.read()
     soup = BeautifulSoup(page, 'lxml')
     s = ''
     for row in range(1, 10):
@@ -15,5 +20,9 @@ if __name__ == '__main__':
             number = 0 if value == '' else int(value)
             s += str(number)
         s += '\n'
-    with open('aaa', 'w') as f:
-        f.write(s)
+    if 'page' in sys.argv:
+        with open('ddd', 'w') as f:
+            f.write(s)
+    else:
+        with open('aaa', 'w') as f:
+            f.write(s)
